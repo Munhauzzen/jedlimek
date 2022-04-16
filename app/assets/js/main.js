d1b2683f71ef0db4deac83aef6b37d6711d1ebe1
Munhauzzen <dvostok1337@gmail.com>
2022-04-16 21:53:35 +0300

    fix+

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
+  $("footer__navLinka").click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    $("body, html").animate({ scrollTop: destination }, 800);
+  });
+
   $('select').styler();
 
-
 });
+
